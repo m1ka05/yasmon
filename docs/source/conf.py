a9116eb1a59e60bf79abec7c2a18ae927dc66f98
Michał Mika <michal@mika.sh>
2023-03-16 00:15:17 +0100

    docs: changed theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,5 +39,5 @@ autodoc_default_options = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'python_docs_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
