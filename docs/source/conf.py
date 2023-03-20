21a51e6e06d26137046bda7a44552151d25d55f0
Michał Mika <michal@mika.sh>
2023-03-20 11:58:44 +0100

    ci: fix release workflow

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'Yasmon'
 copyright = '2023, Michał Ł. Mika'
 author = 'Michał Ł. Mika'
-release = '0.1.2'
+release = '0.1.3'
 version = release
 
 # -- General configuration ---------------------------------------------------
