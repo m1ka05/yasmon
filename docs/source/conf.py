1ea81e937fc3bceb8638a016e5676dcffbf1fdde
Michał Mika <michal@mika.sh>
2023-03-23 20:36:01 +0100

    Bump version: 0.2.1 → 0.2.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'Yasmon'
 copyright = '2023, Michał Ł. Mika'
 author = 'Michał Ł. Mika'
-release = '0.2.1'
+release = '0.2.2'
 version = release
 
 # -- General configuration ---------------------------------------------------
